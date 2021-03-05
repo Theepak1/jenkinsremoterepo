b929a6f37812d5cce9c53f48189004e1fe1061f8
Theepak1 <theepakkprakash@gmail.com>
2021-03-05 12:22:30 +0530

    updated

--- Jenkins/src/com/capg/jenkins/Jenkins.java
+++ Jenkins/src/com/capg/jenkins/Jenkins.java
@@ -5,6 +5,7 @@ public class Jenkins {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("jenkins done");
+		System.out.println("updated");
 
 	}
 
